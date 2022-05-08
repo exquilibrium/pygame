32f784099cd27ff23d9343605a7b486f024ec91e
Mr.Chen <44523875+exquilibrium@users.noreply.github.com>
2022-05-08 11:39:00 +0200

    test

--- test.py
+++ test.py
@@ -12,9 +12,24 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 # Background
-background = pygame.image.load('background.png')
+background = pygame.image.load("background.png")
 
-# main loop
+# Sound
+mixer.music.load("background.wav")
+# mixer.music.play(-1)
+
+# Caption and Icon
+pygame.display.set_caption("Space Invader")
+# icon = pygame.image.load("ufo.png")
+# pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load("player.png")
+playerX = 370
+playerY = 480
+player_change = 0
+
+# Main Loop
 running = True
 while running:
     
@@ -25,6 +40,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_change = -5
 
+    playerX = playerX + player_change  
+
+    screen.blit(playerImg, (playerX, playerY))
+    pygame.display.update()
+
+pygame.quit()
 
